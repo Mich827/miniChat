20125f768f48b1206b9db30f4b21966d90573e1f
Mich827 <100875018+Mich827@users.noreply.github.com>
2024-03-06 19:21:29 +0100

    add meta and title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+    <title>Mini Chat</title>
         <link rel="shortcut icon" href="/icon.ico" />
+    <meta
+          //ajout des meta desc
+          name="description"
+          content="Small messagerie, mini chat for exchange instantly..."
+        />
       </Head>
       <body>
         <Main />
